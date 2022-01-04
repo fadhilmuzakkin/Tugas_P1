3170cd4cedbec2c1de616ff8f69a3f1652e74644
Fadhil Muzakki N <necroambulatory1994@gmail.com>
2022-01-04 23:59:50 +0700

    Latihan 5.4: Melakukan operasi sederhana dengan operator logika

--- learn.js
+++ learn.js
@@ -15,6 +15,7 @@ window.alert("Angka favorit saya adalah : " + myNumber)
 window.alert('Angka favorit saya adalah : ' + myNumber)
 window.alert(`Angka favorit saya adalah : ${myNumber}`)
 
+
 let x = 8 // membuat variabel x dengan nilai 8
 let y = 4 // membuat variabel y dengan nilai 4
 console.log(x, y) // menampilkan nilai x dan y
@@ -28,14 +29,29 @@ console.log('++x =', ++x) //menjumlahkan nilai x dan mengembalikan nilai x
 console.log('x-- =', x--) //menampilkan nilai decrement x
 console.log('--x =', --x) //kebalikan dari increment x
 
+
 let a = 8 // membuat variabel a dengan nilai 8
 let b = 4 // membuat variabel b dengan nilai 4
 console.log(a, b) // menampilkan nilai a dan b
-console.log('a > b =', a > b) // menampilkan true 
-console.log('a < b =', a < b) // menampilkan false 
-console.log('a >= b =', a >= b) // menampilkan true 
-console.log('a <= b =', a <= b) // menampilkan false 
-console.log('a == b =', a == b) // menampilkan false 
-console.log('a != b =', a != b) // menampilkan true 
-console.log('a === b =', a === b) // menampilkan false 
-console.log('a !== b =', a !== b) // menampilkan true 
+console.log('a > b =', a > b) // menampilkan nilai true 
+console.log('a < b =', a < b) // menampilkan nilai false 
+console.log('a >= b =', a >= b) // menampilkan nilai true 
+console.log('a <= b =', a <= b) // menampilkan nilai false 
+console.log('a == b =', a == b) // menampilkan nilai false 
+console.log('a != b =', a != b) // menampilkan nilai true 
+console.log('a === b =', a === b) // menampilkan nilai false 
+console.log('a !== b =', a !== b) // menampilkan nilai true 
+
+
+console.log('true && true =', true && true) // menampilkan nilai true jika keduanya true
+console.log('true && false =', true && false) // menampilkan nilai false jika terdapat false
+console.log('false && true =', false && true) // menampilkan nilai false jika terdapat false
+console.log('false && false =', false && false) // menampilkan nilai false jika keduanya false
+console.log('true || true =', true || true) // menampilkan nilai true jika kedua atau salah satunyanya true
+console.log('true || false =', true || false) // menampilkan nilai true jika terdapat true
+console.log('false || true =', false || true) // menampilkan nilai true jika terdapat true
+console.log('false || false =', false || false) // menampilkan nilai false jika kedua atau salah satunya false
+console.log('!true =', !true) // not true menampilkan nilai false 
+console.log('!false =', !false) // not false menampilkan nilai true 
+
+
